b81770b093f98b434296286c6ef7e24aa4c0ff92
vitthalnamdev <vitthal7804@gmail.com>
2026-03-23 17:59:15 +0530

    Changes involves, setting up the input file feature during file sharing

--- src/clientsCommands/sendingFile.cpp
+++ src/clientsCommands/sendingFile.cpp
@@ -2,41 +2,43 @@
 #include "../../headers/shareFile.hpp"
 #include "../../headers/Status_codes.hpp"
 
+void clear_stdin()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void handle_send_file()
 {
-
-    printf("Enter the I.P that you want to share the file with: ");
     char IP[16];
+    char filename[256];
+    
+    
+    printf("Enter the I.P that you want to share the file with: ");
+    
     if (!fgets(IP, sizeof(IP), stdin))
     {
         perror("Failed to read IP address");
         return;
     }
-
     IP[strcspn(IP, "\n")] = 0;
-
-    char filename[256];
+    clear_stdin();
 
     printf("Enter the file path that you want to share: ");
 
     if (!fgets(filename, sizeof(filename), stdin))
     {
-        perror("Failed to read the filename.");
+        perror("Failed to read filename");
         return;
     }
-
     filename[strcspn(filename, "\n")] = 0;
 
     printf("Sharing file %s with %s...\n", filename, IP);
 
-   
+    int file_sent = send_file(filename, IP);
 
-    int file_sent = send_file(filename , IP);
-
-    if(file_sent > 0){
-        printf("File has been send successfully");
-    }
-    else{
-        printf("There is an error in sending file");
-    }
+    if (file_sent > 0)
+        printf("File has been sent successfully\n");
+    else
+        printf("There is an error in sending file\n");
 }
